100a98c1936bd13f88c97b30c02ee5698ae41744
mahesh <mail.mahesh.lk@gmail.com>
2021-06-11 13:33:21 +0800

    webhook-github -go-webhook lib

---
+++ Main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-playground/webhooks/v6/github"
+	"net/http"
+)
+
+const (
+	path = "/webhooks"
+)
+
+func main() {
+	hook, _ := github.New(github.Options.Secret("git hub secret "))
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn;t one of the ones asked to be parsed
+			}
+		}
+		switch payload.(type) {
+
+		case github.ReleasePayload:
+			release := payload.(github.ReleasePayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", release)
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+	http.ListenAndServe(":3000", nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mahesh-dilhan/go-webhook
+
+go 1.16
+
+require github.com/go-playground/webhooks/v6 v6.0.0-beta.3 // indirect
